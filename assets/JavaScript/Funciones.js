5147e055af557355d3ad99aa05a6bc22ee250567
DanyX55 <muridany03@gmail.com>
2021-11-19 21:28:18 -0500

    e

--- assets/JavaScript/Funciones.js
+++ assets/JavaScript/Funciones.js
@@ -2,5 +2,8 @@ const btnSwitch = document.querySelector(".navbar__switch");
 btnSwitch.addEventListener("click",()=>{
     btnSwitch.classList.toggle("navbar__switch--active");
     document.body.classList.toggle("dark");
+    document.getElementById("fondo").classList.toggle('fade');
+    document.getElementById("fondo").classList.toggle('fade--on');
+    
 }
 )
